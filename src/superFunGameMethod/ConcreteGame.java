7d69b4cf089b9774dae7e31dcf4629bbb03a65b4
reggiegg <reggie.gillett@gmail.com>
2013-10-01 14:49:09 -0700

    Added more personality.

--- src/superFunGameMethod/ConcreteGame.java
+++ src/superFunGameMethod/ConcreteGame.java
@@ -13,8 +13,14 @@ public class ConcreteGame extends AbstractGame {
 		System.out.print("Player One enter your name: ");
 		Scanner scanner = new Scanner(System.in);
 		PlayerOneName = scanner.nextLine();
+		System.out.print("Your name is:");
+		System.out.print(PlayerOneName);
+		System.out.println(". I've never heard that one before.");
 		System.out.print("Player Two enter your name: ");
 		PlayerTwoName = scanner.nextLine();
+		System.out.print("Your name is:");
+		System.out.print(PlayerOneName);
+		System.out.println(". My mother told me if I couldn't say anything nice...");
 		System.out.println("Wow guys, those are great names!");
 		
 	}
@@ -33,7 +39,7 @@ public class ConcreteGame extends AbstractGame {
 			System.out.println(", you are the best! You win!");
 		}
 		if (i == 2){
-			System.out.print(PlayerOneName);
+			System.out.print(PlayerTwoName);
 			System.out.println(", you are the best! You win!");
 		}
 		System.out.println("Truly, you are both winners.");
